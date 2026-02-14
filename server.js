37a84c5ea3f7cd398afd166488afa3bf7454b900
samrat-ghosh-007 <samrat9ghosh2018@gmail.com>
2026-02-14 16:49:56 +0530

    Added Resume Building Logic

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const generateRoute = require("./routes/generate.route");
 const jobRoutes = require("./routes/job.route");
+const resumeRoutes = require("./routes/resume.routes");
+
+
+
 
 const app = express();
+
+
 
 app.use(express.json());
 app.use("/api/generate", generateRoute);
 app.use("/api/jobs", jobRoutes);
+app.use("/api/resume", resumeRoutes);
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
